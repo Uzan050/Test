38f3bd3f85720bf06ff66df115b309dfe538a978
Uzan050 <145941187+Uzan050@users.noreply.github.com>
2023-11-19 20:36:23 +0700

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Destination | Swimming Pool Serang Banten City</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="shortcut icon" href="IMG/favicon.ico.jpg">
+	<link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+</head>
+<body>
+	<nav>
+		<div class="layar-dalam">
+			<div class="logo">
+				<a href=""><img src="IMG/logoo.png" class="putih">
+				<a href=""><img src="IMG/logoo.png" class="hitam">
+			</div>
+			<div class="menu">
+				<a href="#" class="tombol-menu">
+					<span class="garis"></span>
+					<span class="garis"></span>
+					<span class="garis"></span>
+				</a>
+				<ul>
+					<li><a href="#home">Home</a></li>
+					<li><a href="#aboutus">About</a></li>
+					<li><a href="#destinasi">Destinasi</a></li>
+					<li><a href="ourteam.html">Team</a></li>
+					<li><a href="blog.html">Blog</a></li>
+					<li><a href="gallery.html">Gallery</a></li>
+					<li><a href="contact.html">Contact</a></li>
+					<li><a href="logout.php">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="layar-penuh">
+		<header id="home">
+			<div class="intro">
+				<h3>SWIMMING <span class="color">POOL</span></h3>
+<br>
+				<p>Welcome To The Swimming Pool Destination Website</p>
+				<p>
+					<a href="dbatang.html" class="tombol">LIHAT STASTIKA</a>
+				</p>
+			</div>
+		</header>
+		<main>
+			<section id="aboutus">
+				<div class="layar-dalam">
+					<h3>About Us</h3>
+					<p class="ringkasan">Ini Adalah Halaman Kami ( About Us )</p>
+					<div class="konten-isi">
+						<p><strong>Destinasi Kolam Renang</strong> adalah situs yang memberikan informasi seputar kolam renang dan tips seputar aktivitas renang.
+						Di Website Kami Anda dapat menemukan beberapa kolam renang dan lokasi renang yang ada di daerah Banten dan sekitarnyya dengan sangat mudah.
+						Harapan kami adalah semakin banyak masyarakat Serang Banten yang menyukai aktivitas renang dan melakukannya secara rutin, karena efeknya yang sangat bagus untuk kesehatan dan stamina tubuh.
+						Salam,
+						Tim Fznnn</p>
+<br>
+						<p>1. <i>Landing Page</i> Ini adalah bagian pertama halaman website kami, di website kami mempunyai fitur fitur di website kami di antaranya ada Watch Video, Stastik Pengunjung, Halaman About, Kolam Renang Recomend, Gallery, Dan Contact kami.</p>
+
+						<p>2. <i>Watch Video</i> Di dalam Watch Video terdapat video tentang kolam renang yang sangat indah dan nyaman kalian bisa menonton nya dengan durasi 1 menitan saja.</p>
+
+						<p>3. <i>Destinasi</i> Di bagian ke tiga ini "Kolam Renang" adalah untuk melihat recomend kolam renang di daerah banten, Kami menyediakan 5 Destinasi Wisata kolam renang di daerah banten, Kalian bisa cek langsung ke fitur kolam renang.</p>
+
+						<p>4. <i>Gallery</i> Di bagian gallery kami menyediakan beberapa img/gambar kolam renang yang sudah kami buat.</p>
+
+						<p>5. <i>Contact Us</i> Di bagian Contact Us Terdapat Maps Untuk Melihat dimana tempat Destinasi kolam renang Kalian bisa langsung cek.</p>
+<br>
+                		<p><strong>Kesimpulan</strong> Destinasi kolam Renang adalah situs yang memberikan informasi seputar kolam renang dan tips seputar aktivitas renang. Di Website Kami Anda dapat menemukan ribuan data kolam renang dan lokasi renang yang ada di  sekitar Serang Banten dengan sangat mudah. Harapan kami adalah semakin banyak masyarakat indonesia yang menyukai aktivitas renang dan melakukan secara rutin, karena efeknya yang sangat bagus untuk kesehatan dan stamina tubuh.</p>
+
+                		<p>Salam, Tim Fauzan</p>
+					</div>
+				</div>
+			</section>
+			<section class="advertismen">
+				<p>Advertisment Pena Ummah.id</p>
+				<center><a href="https://youtu.be/ZcEmrN4DNwI?si=swNe_JohQ0VaLSca"><img src="https://scontent-cgk1-2.xx.fbcdn.net/v/t1.6435-9/116239409_123342829458749_7143200908913374886_n.jpg?stp=dst-jpg_p843x403&_nc_cat=106&ccb=1-7&_nc_sid=7f8c78&_nc_eui2=AeHmEqki9weJQuGW0RPtRV8rObOIOeAe6vw5s4g54B7q_G0MkcCpjiqC-7qQmbjwx3YER8DhI48cHFKAndJZvUOZ&_nc_ohc=NlwC8ddmTCcAX_WI-Ux&_nc_ht=scontent-cgk1-2.xx&oh=00_AfAj7lSBK_09uMCkmMJhrUJYFFJyAGAhIlWHt4Q0YmSAyg&oe=65742350" alt=""></a></center>
+			</section>
+			<section class="abuabu" id="destinasi">
+				<div class="layar-dalam destinasi">
+					<div>
+						<h6>1. Wisata Aqualand Serang <i>アクアランド</i></h6>
+						
+						<center><img src="IMG/aqualand.jpg"></center>
+						<p>Seluruh bangunan di wisata Aqualand mengusung tema budaya daerah Indonesia. Seperti gapura mirip rumah adat Banten hingga kantin yang berwujud rumah Joglo.
+
+						Fasilitasnya lengkap dengan banyak wahana pada kolam renang seperti ember tumpah maupun perosotan kolam renang. Ada kolam olimpik khusus yang berlatih, olahraga atau kompetisi renang.Aqualand Serang salah satu wisata yang mempunyai kolam renang mewah di Banten.</p>
+
+						<p><b>Lokasi:</b>  Komplek Tegal Padang, Jl. Palem, Kel. Drangong, Kec. Taktakan</p>
+
+						<p><b>Jam Operasional:</b></p>
+						<ul>
+							<li>Senin-Jum'at 08.00-17.00 WIB</li>
+							<li>Sabu-Minggu 08.00 – 18.00 WIB</li>
+						</ul>
+
+						<p><b>Harga Tiket Masuk</b></p>
+						<ul>
+							<li><i class="ri-arrow-right-s-fill">Weekday: Rp 30.000/anak</i></li>
+							<li><i class="ri-arrow-right-s-fill">Weekday: Rp 35.000/dewasa</i></li>
+							<li><i class="ri-arrow-right-s-fill">Weekend: Rp 35.000/anak</i></li>
+							<li><i class="ri-arrow-right-s-fill">Weekend: Rp 40.000/dewasa</i></li>
+							<li><i class="ri-arrow-right-s-fill">High season: Rp 40.000/anak</i></li>
+							<li><i class="ri-arrow-right-s-fill">High season: Rp 45.000/dewasa</i></li>
+						</ul>
+						<b><p class="link">Map:</b> <a href="https://maps.app.goo.gl/9r5WybfRh5g3pJWBA">Klik Disini</a></p>
+						<b><p>No Telp:</b> <a href="tel:+62 812 1901 2400">081219012400</a></p>
+					</div>
+					<div>
+						<h6>2. Kolam Renang Tirta Wiguna Serang <i>ティルタ ウィグナ</i></h6>
+<br>
+						<center><img src="IMG/tirtawiguna.jpg"></center>
+						<p>Kolam renang Tirta Wiguna sebenarnya diperuntukkan bagi para taruna dalam latihan berenang. Tetapi memang dibuka juga untuk umum. Berlokasi di kawasan Kesatuan Grup 1 Kopasus.</p>
+
+						<p><b>Lokasi:</b> Taman Baru, Kec. Taktakan</p>
+
+						<p><b>Jam Operasional: 07.00-17.00 WIB</b></p>
+
+						<p><b>Harga Tiket Masuk</b></p>
+						<ul>
+							<li><i class="ri-arrow-right-s-fill">Masyarakat(Pengunjung) Rp 25.000 per anak</i></li>
+							<li><i class="ri-arrow-right-s-fill">Orang Dewasa Rp 30.000 per orang dewasa</i></li>
+						</ul>
+						<b><p>Map:</b> <a href="https://maps.app.goo.gl/gCJaacMi7rUoE1bj7">Klik Disini</a></p>
+						<b><p>No Telp:</b> <a href="tel:+62 877 7238 4998">087772384998</a></p>
+					</div>
+					<div>
+						<h6>3. Kolam Renang Tirta Nada Serang <i>ティルタ・ナダ</i></h6>
+<br>
+						<center><img src="IMG/tirtanada.jpg"></center>
+						<p>Selain kolam renang, Tirta Nada juga memiliki mini zoo dan bagian dari “nada” alias live music.</p>
+
+						<p><b>Lokasi:</b>Lebakwangi, Kec. Walantaka</p>
+
+						<p><b>Jam Operasional:</b> 07.00 – 17.00 WIB</p>
+
+						<p><b>Harga Tiket Masuk:</b></p>
+						<ul>
+							<li><i class="ri-arrow-right-s-fill">HTM kolam renang Rp15.000</i></li>
+							<li><i class="ri-arrow-right-s-fill">Tiket terusan ke taman + Rp5.000</i></li>
+						</ul>
+						<b><p>Map:</b> <a href="https://maps.app.goo.gl/pd9NBzrno3ihHCgH8">Klik Disini</a></p>
+						<b><p>No Telp:</b> <a href="tel:+62 821 2508 9779">082125089779</a></p>
+					</div>
+					<div>
+						<h6>4. Kolam Renang Bambo Park <i>竹公園</i></h6>
+<br>
+						<center><img src="IMG/bambopark.jpg"></center>
+						<p>Selain berenang, Anda bisa menikmati spot foto termasuk miniatur Menara Eiffel hingga bumblebee, atau duduk santai di rooftop.</p>
+
+						<p><b>Lokasi:</b> Jl. Kramtwatu, Sukadalem, Kec. Waringinkurung</p>
+
+						<p><b>Jam Operasional:</b> 08.00 – 18.00 WIB</p>
+
+						<p><b>Harga Tiket Masuk:</b></p>
+						<ul>
+							<li><i class="ri-arrow-right-s-fill">Weekday Rp20.000</i></li>
+							<li><i class="ri-arrow-right-s-fill">Weekend & libur Rp30.000</i></li>
+						</ul>
+						<b><p>Map:</b> <a href="https://maps.app.goo.gl/e53cKU1QAQPnTWbt7">Klik Disini</a></p>
+						<b><p>No Telp:</b> <a href="tel:+62 81 788 5053">0817885053</a></p>
+					</div>
+					<div>
+						<h6>5. Kolam Renang Sport Club Serang City <i>スポーツクラブ</i></h6>
+<br>
+						<center><img src="IMG/sportclub.jpg"></center>
+						<p>Sebagai fasilitas dari perumahan Serang City, kolam renang ini memunculkan kesan private pool.</p>
+
+						<p><b>Lokasi:</b> Kompleks Serang City, Kel. Drangong, Kec. Taktakan</p>
+
+						<p><b>Jam Operasional:</b> 08.00 – 18.00 WIB</p>
+
+						<p><b>Harga Tiket Masuk:</b></p>
+						<ul>
+							<li><i class="ri-arrow-right-s-fill">Warga Serang City Rp15.000 (<12 tahun) dan Rp20.000 (dewasa)</i></li>
+							<li><i class="ri-arrow-right-s-fill">Umum Rp20.000 (les/latihan) dan Rp30.000 (anak & dewasa)</i></li>
+						</ul>
+						<b><p>Map:</b> <a href="https://maps.app.goo.gl/7zj5oSqd4E7A4WQk9">Klik Disini</a></p>
+						<b><p>No Telp:</b> <a href="tel:+62 825 422 0852">08254220852</a></p>
+					</div>
+				</div>
+			</section>
+			<section id="gallery">
+				<div><img src="IMG/gallery1.jpg"></div>
+				<div><img src="IMG/gallery2.jpg"></div>
+				<div><img src="IMG/gallery3.jpg"></div>
+				<div><img src="IMG/gallery4.jpg"></div>
+				<div><img src="IMG/gallery5.jpg"></div>
+				<div><img src="IMG/gallery6.jpg"></div>
+			</section>
+			<section class="tour">
+				<div class="section_container tour_container">
+					<h4>Swimming Pool Videos</h4>
+					<div class="video_container">
+						<video src="IMG/watch01.mp4" autoplay loop muted></video>
+					</div>
+				</div>
+			</section>
+
+		</main>
+			<footer class="footer">
+			<div class="container">
+				<div class="row">
+					<div class="footer-col">
+						<h4>company</h4>
+						<ul>
+							<li><a href="#">about us</a></li>
+							<li><a href="#">our services</a></li>
+							<li><a href="#">privacy policy</a></li>
+							<li><a href="">Affliate Program</a></li>
+						</ul>
+					</div>
+					<div class="footer-col">
+						<h4>get help</h4>
+						<ul>
+							<li><a href="#">FAQ</a></li>
+							<li><a href="#">shipping</a></li>
+							<li><a href="#">return</a></li>
+							<li><a href="#">order status</a></li>
+							<li><a href="">payment options</a></li>
+						</ul>
+					</div>
+					<div class="footer-col">
+						<h4>online shop</h4>
+						<ul>
+							<li><a href="#">watch</a></li>
+							<li><a href="#">destination</a></li>
+							<li><a href="#">swimming pool</a></li>
+							<li><a href="#">dress</a></li>
+						</ul>
+					</div>
+					<div class="footer-col">
+						<h4>Destination</h4>
+						<div class="social-links">
+							<!-- <a href="#"><i class="ri-facebook-box-fill"></i></a>
+							<a href="#"><i class="ri-twitter-x-fill"></i></a>
+							<a href="#"><i class="ri-google-line"></i></a> -->
+							<img src="IMG/favicon.ico.jpg" alt="">
+							<img src="IMG/smkn7.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="footer-bottom">
+				<p>&copy;2023 DestinasiKolamRenang | <span>Banten</span></p>
+			</div>
+		</footer>
+	</div>
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+	<script src="javascript.js"></script>
+</body>
+</html>
